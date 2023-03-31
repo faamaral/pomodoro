54a14b4da95895beb6c7681ebce8e1ad73303a8c
faamaral <fabianoamaral445@gmail.com>
2023-03-31 11:15:01 -0300

    first commit with pomodoro page and cronometro and entrada_tempo components

---
+++ android/app/src/main/kotlin/com/faamaral/pomodoro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.faamaral.pomodoro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/pages/pomodoro.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pomodoro',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+      ),
+      home: const Pomodoro(),
+    );
+  }
+}
+
+// final store = ContadorStore();
+
+// class MyWidget extends StatelessWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+        
+//         title: Text('contador'),
+//       ),
+//       body: Center(
+        
+//         child: Column(
+          
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Observer(
+//               builder: (_) => Text(
+//                 '${store.contador}',
+//                 style: Theme.of(context).textTheme.headline4,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: store.incrementar,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
